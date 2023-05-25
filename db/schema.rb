89d00cc3f5d8132b7f818647356abe613a8d9c9e
absyah <ardian.bahtiarsyah@gmail.com>
2023-05-25 08:37:35 +0700

    add guest model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_25_013331) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_013713) do
+  create_table "guests", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_numbers"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "reservations", force: :cascade do |t|
     t.string "reservation_code"
     t.date "start_date"
